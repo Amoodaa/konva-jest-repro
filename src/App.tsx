581de54670b0f360b5907e33dedd786109a3b2c7
Imad Shatali <imad.shatali@gmail.com>
2022-08-22 15:35:47 +0300

    repro initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+import { Stage, Layer, Circle } from "react-konva";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Stage width={300} height={400} draggable>
+          <Layer>
+            <Circle fill="blue" x={100} y={150} width={200} height={200} />
+          </Layer>
+        </Stage>
       </header>
     </div>
   );
